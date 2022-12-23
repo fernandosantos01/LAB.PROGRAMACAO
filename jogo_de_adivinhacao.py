5800630c18e39ba187fcb32bde7909ad3b0fd908
José Fernando <josefernandojosefernando.09@gmail.com>
2022-12-23 19:38:14 -0300

    Atualizações
    Adicionei uma função para determinar a quantidade de tempo de execução do programa e adicionei menus de opções para os níveis  do jogo e para saber se o usuário deseja continuar jogando

--- jogo_de_adivinhacao.py
+++ jogo_de_adivinhacao.py
@@ -1,7 +1,10 @@
 import random
 import sys
+import time
 
 #acertou = False
+
+contador_de_jogadas = 0
 
 contador_para_chances = 0
 
@@ -10,60 +13,70 @@ contador_para_resultado = 0
 
 numero_secreto = random.randint(1,100)
 
-nome_usuario = input("Qual o seu nome?\n")
+while True:
 
-nivel = input("Qual o nivel?\n")
+    print(f'*****Bem vindo ao Jogo de Adivinhação*****\n')
+    inicio = time.time()
+    nome_usuario = input("Qual o seu nome?\n")
 
-nivel_do_jogo = int (nivel)
+    print('—- Menu de Opções —-')
+    nivel = input('F - Fácil\nM - Médio\nD - Difícil\nQual a sua opção?\n')
 
-if(nivel_do_jogo == 1):
-  tentativas = 3
-if(nivel_do_jogo == 2):
-  tentativas = 5
-if(nivel_do_jogo == 3):
-  tentativas = 6
+    nivel_do_jogo = nivel
 
-contador_para_chances = tentativas
+    if(nivel_do_jogo == "F"):
+      tentativas = 10
+    if(nivel_do_jogo == "M"):
+      tentativas = 5
+    if(nivel_do_jogo == "D"):
+      tentativas = 3
 
-for i in range(0,tentativas):
+    contador_para_chances = tentativas
 
-  print(f'\nSeu numero de chances é {contador_para_chances}\n')
-  contador_para_chances = contador_para_chances -1
+    for i in range(0,tentativas):
 
-  #Serve para contabilizar o numero de chances restantes do usuário
-  contador_para_resultado = contador_para_resultado +1
-  
-#while(acertou not)
-  
-  palpite_usuario = input("Qual o seu palpite ?\n")
+      print(f'Seu numero de chances é {contador_para_chances}')
+      contador_para_chances = contador_para_chances -1
 
-  #Essa linha de codigo serve para a conversão de string para   tipo inteiro 
-  palpite = int(palpite_usuario)
+      #Serve para contabilizar o numero de chances restantes do usuário
+      contador_para_resultado = contador_para_resultado +1
+      
+    #while(acertou not)
+      
+      palpite_usuario = input("Qual o seu palpite ?\n")
 
-    #A letra 'f' serve para melhorar a formatação
-  print(f'\nO usuario {nome_usuario} deu o palpite {palpite}\n')
-    
-  if(palpite == numero_secreto):
-    print(f'{nome_usuario} você acertou o número       {numero_secreto}')
-    
-    sys.exit("Programa encerrado")
-    #acertou = True
+      #Essa linha de codigo serve para a conversão de string para   tipo inteiro 
+      palpite = int(palpite_usuario)
+      contador_de_jogadas = contador_de_jogadas+1
+        #A letra 'f' serve para melhorar a formatação
+      print(f'O usuario {nome_usuario} deu o palpite {palpite}')
+      if(palpite == numero_secreto):
+        print(f'{nome_usuario} você acertou o número {numero_secreto}, a quantidade de jogadas foram {contador_de_jogadas}')
+        fim = time.time()
+        print ('duracao: %.2f segundos' % (fim - inicio))
+        break
+        
+        #sys.exit("Programa encerrado")
+        #acertou = True
 
-  else:
-    print(f'{nome_usuario} você errou o número secreto!\n')
-    
-    #Linha de código comentada a seguir foi ultilizada para fazer o teste do encerramento instataneo
-     
-    #print(f'{nome_usuario} O numero correto era {numero_secreto}!')
-    
-    #acertou = False
-    
-  if(palpite<numero_secreto):
-    print(f'{nome_usuario} Você errou pra baixo, Portanto digite um numero maior\n')
+      else:
+        print(f'{nome_usuario} você errou o número secreto!')
 
-  else:
-    print(f'Você errou para cima, Portanto digite um numero menor\n')
+        #teste de resultado
+        #print(f'{nome_usuario} O numero correto era {numero_secreto}!')
+        
+        #acertou = False
+        
+      if(palpite<numero_secreto):
+        print(f'{nome_usuario} Você errou pra baixo, Portanto digite um numero maior')
+      if(palpite>numero_secreto):
+        print(f'Você errou para cima, Portanto digite um numero menor')
+        
 
-  if(contador_para_resultado==tentativas):
-    print(f'{nome_usuario} O numero correto era {numero_secreto}!\n')
-    
+      if(contador_para_resultado==tentativas):
+        print(f'{nome_usuario} O numero correto era {numero_secreto}!')
+      
+    opcao = input ("Deseja continuar? [1 - Sim || 2 - Não]\n")
+    opc = int(opcao)
+    if(opc == 2):
+      break
